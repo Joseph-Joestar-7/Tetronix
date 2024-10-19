f13eb050fac83c3705b79f6d8f1523fee7e5100f
Ritaban Chaudhuri <inrcr27@gmail.com>
2024-10-19 22:16:33 +0530

    Added Entity.cpp

---
+++ src/Entity.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Components.hpp"
+#include <memory>
+#include <string>
+
+class Entity{
+	friend class EntityManager; 
+	bool m_active = true;
+	size_t m_id = 0;
+	std::string m_tag = "default";
+
+	Entity(const size_t id, const std::string& tag);
+public:
+	std::shared_ptr<CTransform> cTransform;
+	std::shared_ptr<CShape> cShape;
+	std::shared_ptr<CCollision> cCollision;
+	std::shared_ptr<CScore> cScore;
+	std::shared_ptr<CInput> cInput;
+
+	bool isActive() const;
+	const std::string& tag() const;
+	const size_t id() const;
+};
+
